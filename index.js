6ae477a11a4cdf7a438b26296355027213ac0cd9
David Gifford <giff@ncirql.com>
2019-04-23 11:44:58 -0400

    More option changes

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = uglifyjs;
 
 
 function uglifyjs ( code, options ) {
-  options.outSourceMap = options.sourceMap !== false;
 
   console.log(`terser Options: {}`, options);
   let minified  = terser.minify( code, options );
